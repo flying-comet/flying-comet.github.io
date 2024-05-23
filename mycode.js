de62c9f8c808144977fd68b4a5436b8abc7d0a79
flying-comet <kieru@tutanota.com>
2024-05-22 23:14:51 -0400

    Update mycode.js

--- mycode.js
+++ mycode.js
@@ -1,30 +1,35 @@
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
+function setCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
 
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
+function getCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
 
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
-onload = function () {
-    document.getElementById('linksNewWindow').checked = getCookie('linksNewWindow') == 1 ? true : false;
-}
-function set_check() {
-    setCookie('linksNewWindow', document.getElementById('linksNewWindow').checked ? 1 : 0, 100);
-}
+function rememberCheckboxState() {
+      var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+      for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', function() {
+          setCookie(this.id, this.checked, 365);
+        });
+        var checkboxState = getCookie(checkboxes[i].id);
+        if (checkboxState !== null) {
+          checkboxes[i].checked = checkboxState === "true";
+        }
+      }
+    }
+
+rememberCheckboxState();
